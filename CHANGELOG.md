163f8b3994800aa5bbd4142fb9f76570fb95aeab
Ryan Morrissey <23maverick23@gmail.com>
2025-07-07 08:23:11 -0600

    Fixed typing for plugin submission

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.2] - 2025-07-07
+
+### Fixed
+
+- Fixed `any` typing in settings.ts and save-interceptor.ts to comply with linting.
+
 ## [0.1.1] - 2025-07-06
 
 ### Added
@@ -49,9 +55,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - LICENSE file to enable community contributions.
 - README to explain the plugin and how to use it.
 
-[unreleased]: https://github.com/23maverick23/attendees-extractor/compare/0.1.1...HEAD
+[unreleased]: https://github.com/23maverick23/attendees-extractor/compare/0.1.2...HEAD
 [0.0.1]: https://github.com/23maverick23/attendees-extractor/releases/tag/0.0.1
 [0.0.2]: https://github.com/23maverick23/attendees-extractor/releases/tag/0.0.2
 [0.0.3]: https://github.com/23maverick23/attendees-extractor/releases/tag/0.0.3
 [0.1.0]: https://github.com/23maverick23/attendees-extractor/releases/tag/0.1.0
 [0.1.1]: https://github.com/23maverick23/attendees-extractor/releases/tag/0.1.1
+[0.1.2]: https://github.com/23maverick23/attendees-extractor/releases/tag/0.1.2
--- src/save-interceptor.ts
+++ src/save-interceptor.ts
@@ -1,47 +1,68 @@
-import { App, TFile } from 'obsidian';
-import { AttendeesExtractorSettings } from './types';
-import { FileUtils } from './file-utils';
+import { App, TFile } from "obsidian";
+import { AttendeesExtractorSettings } from "./types";
+import { FileUtils } from "./file-utils";
+
+interface ObsidianApp extends App {
+	commands: {
+		commands: {
+			[key: string]: {
+				checkCallback?: (checking: boolean) => boolean;
+			};
+		};
+	};
+}
 
 export class SaveInterceptor {
-  private originalSaveCallback: ((checking: boolean) => boolean) | null = null;
+	private originalSaveCallback: ((checking: boolean) => boolean) | null =
+		null;
 
-  constructor(
-    private app: App,
-    private settings: AttendeesExtractorSettings,
-    private onFileSave: (file: TFile) => void
-  ) {}
+	constructor(
+		private app: App,
+		private settings: AttendeesExtractorSettings,
+		private onFileSave: (file: TFile) => void
+	) {}
 
-  setup(): void {
-    const saveCommand = (this.app as any).commands.commands['editor:save-file'];
-    if (!saveCommand) return;
+	setup(): void {
+		const obsidianApp = this.app as ObsidianApp;
+		const saveCommand = obsidianApp.commands.commands["editor:save-file"];
+		if (!saveCommand) return;
 
-    if (!this.originalSaveCallback) {
-      this.originalSaveCallback = saveCommand.checkCallback;
-      saveCommand.checkCallback = (checking: boolean) => {
-        if (checking) {
-          return this.originalSaveCallback!(checking);
-        } else {
-          this.originalSaveCallback!(checking);
-          this.handleFileSave();
-        }
-      };
-    }
-  }
+		if (!this.originalSaveCallback) {
+			this.originalSaveCallback = saveCommand.checkCallback || null;
+			saveCommand.checkCallback = (checking: boolean) => {
+				if (checking) {
+					return this.originalSaveCallback
+						? this.originalSaveCallback(checking)
+						: true;
+				} else {
+					if (this.originalSaveCallback) {
+						this.originalSaveCallback(checking);
+					}
+					this.handleFileSave();
+					return true;
+				}
+			};
+		}
+	}
 
-  remove(): void {
-    const saveCommand = (this.app as any).commands.commands['editor:save-file'];
-    if (saveCommand && this.originalSaveCallback) {
-      saveCommand.checkCallback = this.originalSaveCallback;
-      this.originalSaveCallback = null;
-    }
-  }
+	remove(): void {
+		const obsidianApp = this.app as ObsidianApp;
+		const saveCommand = obsidianApp.commands.commands["editor:save-file"];
+		if (saveCommand && this.originalSaveCallback) {
+			saveCommand.checkCallback = this.originalSaveCallback;
+			this.originalSaveCallback = null;
+		}
+	}
 
-  private handleFileSave(): void {
-    if (!this.settings.enableOnSave) return;
+	private handleFileSave(): void {
+		if (!this.settings.enableOnSave) return;
 
-    const file = this.app.workspace.getActiveFile();
-    if (FileUtils.isMarkdownFile(file) && FileUtils.isFileInAllowedDirectories(file, this.settings)) {
-      this.onFileSave(file);
-    }
-  }
+		const file = this.app.workspace.getActiveFile();
+		if (
+			FileUtils.isMarkdownFile(file) &&
+			FileUtils.isFileInAllowedDirectories(file, this.settings)
+		) {
+			this.onFileSave(file);
+		}
+	}
 }
